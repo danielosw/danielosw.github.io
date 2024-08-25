d1bc68610046a40becd8af19c731e03873487dc0
danielosw <65432065+danielosw@users.noreply.github.com>
2024-08-24 23:53:23 -0500

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,11 @@
 <script lang="ts">
-import viteLogo from "/vite.svg";
-import svelteLogo from "./assets/svelte.svg";
-import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="flex">
+    <p class="px-4 py-2 rounded-full bg-purple-500 text-white flex-1">
+      Hello! I am Daniel, and welcome to my site.
+    </p>
+
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
