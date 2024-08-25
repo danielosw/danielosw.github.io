2a6b1d8b85c10aa2a7617a608f0d80910a7e73a6
danielosw <65432065+danielosw@users.noreply.github.com>
2024-08-25 16:15:14 -0500

    remake bee calculator and add it

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,14 @@
     <p class="px-4 py-2 rounded-full bg-purple-500 text-white flex-1">
       Hello! I am Daniel, and welcome to my site.
     </p>
-
+    
+  </div>
+  <div class="flex justify-center">
+    <ul class="">
+      <div class="px-2 py-1 rounded-full bg-violet-500 text-white">My Projects</div>
+      <li class="bg-red-500 rounded-full px-2 py-1">
+        <a href="/huntingbees"> fallen london calculators</a>
+      </li>
+    </ul>
   </div>
 </main>
