fd2299b7902e385a921ee4aa09a79252733fb35a
danielosw <65432065+danielosw@users.noreply.github.com>
2024-08-25 20:32:05 -0500

    hompe

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,14 @@
-<script lang="ts">
+<script>
+    import Router from "svelte-spa-router";
+    import Home from "./routes/Home.svelte";
+    import HuntingBees from "./routes/HuntingBees.svelte";
+
+    const routes = {
+        "/": Home,
+        "/huntingbees": HuntingBees,
+    };
 </script>
 
 <main>
-  <div class="flex">
-    <p class="px-4 py-2 rounded-full bg-purple-500 text-white flex-1">
-      Hello! I am Daniel, and welcome to my site.
-    </p>
-    
-  </div>
-  <div class="flex justify-center">
-    <ul class="">
-      <div class="px-2 py-1 rounded-full bg-violet-500 text-white">My Projects</div>
-      <li class="bg-red-500 rounded-full px-2 py-1">
-        <a href="/huntingbees"> fallen london calculators</a>
-      </li>
-    </ul>
-  </div>
+    <Router {routes} />
 </main>
