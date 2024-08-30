fdf1ac0079c7033242573ea2def87f25f32461dc
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-08-30 14:44:44 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 import "../app.css";
 </script>
-<main class="">
-    <div class="flex justify-center items-center">
+<main class="justify-center self-center">
+    <div class="flex justify-center self-center">
         <p class="px-4 py-2 rounded-full bg-purple-500 text-white">
             Hello! I am Daniel, and welcome to my site.
         </p>
     </div>
-    <div class="flex justify-center items-center">
+    <div class="flex justify-center self-center">
         <ul class="">
             <div class="px-2 py-1 rounded-full bg-violet-500 text-white text-center">
                 My Projects
