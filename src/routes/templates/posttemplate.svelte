8832275ecb053d251d82071196cf96fa61541c37
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-09-20 14:37:10 -0500

    push

--- src/routes/templates/posttemplate.svelte
+++ src/routes/templates/posttemplate.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-export let text = "placeholder";
-export let title = "placeholder";
+    export let title = "placeholder";
 </script>
+
 <li>
-<div class="blogbox">
-    <h1 class="title">
-        {title}
-    </h1>
-    <br>
-    <p>
-        {text}
-    </p>
-</div>
+    <div class="blogbox">
+        <h1 class="title">
+            {title}
+        </h1>
+        <br />
+        <p>
+            <slot></slot>
+        </p>
+    </div>
 </li>
