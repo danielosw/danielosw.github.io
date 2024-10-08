2f5a5ee2c68c4c58bd209f069d5cb2888970f3f3
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-08 14:48:33 -0500

    use postcss-preset-env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,17 +1,22 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
+const postcssPresetEnv = require('postcss-preset-env');
 module.exports = {
 	plugins: [
+
 		require("postcss-import"),
 		require("postcss-nesting"),
-
 		require("tailwindcss"),
-		require("autoprefixer"),
-		require("cssnano")({
-			preset: "advanced",
+		postcssPresetEnv({
+			/* pluginOptions */
+			features: {stage: 3 },
+			autoprefixer: { grid: false },
 		}),
 		purgecss({
 			content: ["./src/**/*.html", "./src/**/*.svelte"],
 			defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+		}),
+		require("cssnano")({
+			preset: "advanced",
 		}),
 	],
 };
