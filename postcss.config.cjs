07603b7f02ebea9b092a2341b2b2d3927d91d06c
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-09 12:15:01 -0500

    fix favicon, bump and lint

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,18 +1,20 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
-const postcssPresetEnv = require('postcss-preset-env');
+const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
 	plugins: [
-
 		require("postcss-import"),
 		require("postcss-nesting"),
 		require("tailwindcss"),
 		postcssPresetEnv({
 			/* pluginOptions */
-			features: {stage: 3 },
+			stage: 3,
 			autoprefixer: { grid: false },
 		}),
 		purgecss({
 			content: ["./src/**/*.html", "./src/**/*.svelte"],
+			css: ["./**/*.css"],
+			//defaultExtractor: (content) => content.match("class=\"([\\w\\-/: ]+)\"(?<!:)|id=\"([\\w\\-/: ]+)\"(?<!:)|<([\\w\\-/: ]+) (?<!:)|<\\/([\\w\\-/: ]+)>(?<!:)") || [],
+
 			defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 		}),
 		require("cssnano")({
