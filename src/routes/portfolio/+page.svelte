fef829a97b3077fe16a4f9307fdd432794bdb476
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-10 14:42:19 -0500

    start on portfolio

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import "$lib/css/app.css";
+import Portspot from "../templates/portfoliospot.svelte";
+</script>
+<main class="flex max-w-full max-h-full">
+    <div class="virtgrid fade2s">
+    <div class="portwrap">
+    <ul>
+        <Portspot title="ai thoughts" href="/" imgsrc="/favicon.png">
+            A post about my thoughts on ai.
+        </Portspot>
+    </ul>
+</div>
+</div>
+    </main>
---
+++ src/routes/templates/portfoliospot.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import "$lib/animations/fade.css";
+    import "./portfoliospot.css";
+    export let title = "placeholder";
+    export let href = "placeholder";
+    export let imgsrc = "placeholder";
+    export let alt = "placeholder";
+</script>
+<div class="fade1s">
+    <a {href}>
+        <img {alt} src="{imgsrc}">
+        <h1>{title}</h1>
+        <p>
+            <slot></slot>
+        </p>
+    </a>
+</div>
