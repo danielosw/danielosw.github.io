3a5e347688cdb1155bcdcd98a4d02a9e0efe0225
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-11-20 12:27:21 -0600

    try to add a glossy look to buttons

---
+++ src/lib/templates/gloss.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "$lib/css/gloss.css";
+	export let i = "";
+	</script>
+		<!-- -->
+	<main class="">
+		<div class="{i}">
+			<slot class=""></slot>
+		</div>
+
+	</main>
+	
+	
