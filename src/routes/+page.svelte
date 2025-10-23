1a1d288112ebe0ccbe689e14303e7453cb0639cf
danielosw <65432065+danielosw@users.noreply.github.com>
2025-10-23 11:24:51 -0500

    remove not working gloss effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import "./index.css";
-	import Gloss from "$lib/templates/gloss.svelte";
 </script>
 
 <main class="">
@@ -14,21 +13,15 @@
 			<li>
 				<div class="text-item fade2s">My Projects</div>
 			</li>
-			<Gloss i="gloss">
-				<li class="link-item fade3s">
-					<a href="/huntingbees"> fallen london calculators</a>
-				</li>
-			</Gloss>
-			<Gloss i="gloss">
-				<li class="link-item fade4s">
-					<a href="/blog"> My blog</a>
-				</li>
-			</Gloss>
-			<Gloss i="gloss">
-				<li class="link-item fade5s">
-					<a href="/portfolio"> My portfolio</a>
-				</li>
-			</Gloss>
+			<li class="link-item fade3s">
+				<a href="/huntingbees"> fallen london calculators</a>
+			</li>
+			<li class="link-item fade4s">
+				<a href="/blog"> My blog</a>
+			</li>
+			<li class="link-item fade5s">
+				<a href="/portfolio"> My portfolio</a>
+			</li>
 		</ul>
 	</div>
 </main>
