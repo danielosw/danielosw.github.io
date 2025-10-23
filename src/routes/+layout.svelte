61cdb61a949984f7ad3436b69524a5a4326b6e6a
danielosw <65432065+danielosw@users.noreply.github.com>
2025-10-23 12:45:33 -0500

    render sidebar with inputed info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,21 @@
 	import Footer from "./templates/footer.svelte";
 	import Sidebar from "./templates/sidebar.svelte";
 	interface Props {
-		children?: import('svelte').Snippet;
+		children?: import("svelte").Snippet;
 	}
 
 	let { children }: Props = $props();
 </script>
 
 {@render children?.()}
-<Sidebar></Sidebar>
+<Sidebar>
+	<li class="droplink fade1"><a href="/">Home</a></li>
+	<li class="droplink fade2"><a href="/blog">Blogs</a></li>
+	<li class="droplink fade3">
+		<a href="/huntingbees">Calculators</a>
+	</li>
+	<li class="droplink fade4">
+		<a href="/portfolio">Portfolio</a>
+	</li>
+</Sidebar>
 <Footer></Footer>
-
