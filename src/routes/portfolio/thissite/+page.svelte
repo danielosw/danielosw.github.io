a81835151657f49196ee65de455e11f21a0a4ec4
danielosw <65432065+danielosw@users.noreply.github.com>
2025-10-23 14:08:42 -0500

    add first portfolio

---
+++ src/routes/portfolio/thissite/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageProps } from "./$types";
+	import Post from "$lib/templates/detailtemplate.svelte";
+
+	let { data }: PageProps = $props();
+</script>
+
+<Post title="This site">
+	<p>
+		This site, danielosw.github.io, has been a long term project that I work
+		away at from time to time. I started it to learn how to build a site and
+		I think it has been pretty good at that.
+	</p>
+</Post>
